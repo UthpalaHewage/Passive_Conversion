a17e5a292c6456ac942f60be89853322ae6e226b
UthpalaHewage <uthpalahewage11@gmail.com>
2020-01-18 16:37:46 +0530

    test shuffle sentence

--- sent_test.py
+++ sent_test.py
@@ -1,7 +1,7 @@
 import spacy
 
 nlp = spacy.load('en_core_web_sm')
-doc = nlp(u"")
+doc = nlp(u"We looked at heavy nouns in the first lecture this week, so you might want to go back and check that section.")
 for i in range(len(doc)):
     print(f'{i:{10}} {doc[i].text:{10}} {doc[i].pos_:{8}} {doc[i].tag_:{6}} {doc[i].dep_:{10}} {spacy.explain(doc[i].tag_)}')
 
--- shuffle.py
+++ shuffle.py
@@ -1,43 +1,36 @@
-"""Make the transcript organized manner, segment the content sentence vice """
-
 # Import spaCy and load the language library
 import spacy
 from nltk import tokenize
 from random import shuffle
 
 import Model.paragraph as para
-# import contraction_removal
 
 # load small version of english library
 # python -m spacy download en_core_web_sm
 nlp = spacy.load('en_core_web_sm')
 
 
-class SentenceSegmentation(object):
-    """class for the sentence segmentation of the transcript"""
-
-    # import the method for the conversion of the contracted words into expanded form
-    # contraction_removal_obj = contraction_removal.ContractionRemoval()
+class Shuffle(object):
 
     def __init__(self):
         pass
 
-    def sent_segment(self):
-        """segmenting the sentence in the transcript"""
+    def shuffle(self):
+        # with open('files/tense conversion_2.txt', 'r') as file:
         with open('test.txt', 'r') as file:
-        # with open('files/collection/test1.txt', 'r') as file:
-        # with open('files/collection/selected 6 transcripts/5_2.Forms and functions of pronouns.txt','r') as file:
+            print("check1")
             # read the text file_transcript
             data = file.read()
             # tokenize the sent and replace the uneven line breaks
         all_sent_list = tokenize.sent_tokenize(data.replace("\n", " "))
-
+        print("check2")
         # used to rewrite a file after shuffling for evaluation
         shuffle(all_sent_list)
-        with open('your_file.txt', 'w') as f:
+        print("check3")
+        with open('new_list.txt', 'w') as f:
             for item in all_sent_list:
                 f.write("%s\n" % item)
-        # self.contraction_removal_obj.expand_contractions(all_sent_list)
+            print("check4")
         self.print_para()
 
     @staticmethod
